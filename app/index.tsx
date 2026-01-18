267677a49045596391362606dbc6a90215e30ac3
kkamara <kamaracomputers@gmail.com>
2026-01-18 11:54:15 +0000

    Initial file-based routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -8,6 +9,8 @@ export default function Index() {
       <Text className="text-5xl text-dark-200 font-bold">
         Welcome.
       </Text>
+      <Link href="/onboarding">Onboarding</Link>
+      <Link href="/movie/avengers">Avengers Movie</Link>
     </View>
   );
 }
